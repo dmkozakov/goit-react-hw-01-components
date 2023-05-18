2a9351af9ca4a0b5fc708efe67b8767ea4c9cb97
Dmytro Kozakov <dimakozakov1999@gmail.com>
2023-05-18 13:04:09 +0300

    Initial copy

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,14 +3,14 @@ import css from './Statistics.module.css';
 
 export function Statistics({ title, stats }) {
   return (
-    <section class={css.statistics}>
-      {title && <h2 class={css.title}>{title}</h2>}
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
 
-      <ul class={css.stat__list}>
+      <ul className={css.stat__list}>
         {stats.map(stat => (
-          <li class={css.item} key={stat.id}>
-            <span class={css.label}>{stat.label}</span>
-            <span class={css.percentage}>{stat.percentage}%</span>
+          <li className={css.item} key={stat.id}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
           </li>
         ))}
       </ul>
