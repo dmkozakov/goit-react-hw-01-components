5a8f43acc6802130a8a1ce633aeff62845b81f50
Dmytro Kozakov <dimakozakov1999@gmail.com>
2023-05-19 13:02:39 +0300

    change width for FriendList and transaction history

--- src/components/TransactionHistory/TransactionHistory.styled.jsx
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -1,11 +1,12 @@
 import styled from '@emotion/styled';
 
 export const Table = styled.table`
-  width: 80%;
+  width: 600px;
 
   margin-left: auto;
   margin-right: auto;
   margin-top: 40px;
+  margin-bottom: 40px;
 
   background-color: #fff;
 `;
