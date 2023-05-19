9d87290c8706d34ae0d75bba23f5d7e4996b7f92
Dmytro Kozakov <dimakozakov1999@gmail.com>
2023-05-19 12:58:24 +0300

    Add task 4

---
+++ src/components/TransactionItem/TransactionItem.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Row = styled.tr`
+  /* display: table-cente; */
+  height: 36px;
+  text-align: center;
+
+  :nth-child(even) {
+    background-color: #e2dcdc;
+  }
+`;
